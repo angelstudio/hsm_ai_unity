722d4a7f54c07e360412b3170d8424e9a229f1f3
Hasumi <skytea922@gmail.com>
2018-11-06 23:08:48 +0900

    :construction: テキスト入力を取得

---
+++ Assets/InputManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class InputManager : MonoBehaviour
+{
+
+    InputField inputField;
+    public string inputValue = "";
+
+
+    // 初期化
+    void Start()
+    {
+        inputField = GetComponent<InputField>();
+
+        InitInputField();
+    }
+
+    // ログ出力
+    public void InputLogger()
+    {
+        inputValue = inputField.text;
+
+        Debug.Log(inputValue);
+
+        InitInputField();
+    }
+
+    // フィールドを初期化してフォーカスを当てる
+    void InitInputField()
+    {
+        // 値をリセット
+        inputField.text = "";
+
+        // フォーカス
+        inputField.ActivateInputField();
+    }
+
+}
---
+++ Assets/OutputManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OutputManager : MonoBehaviour {
+
+    Text text;
+    public InputField inputField;
+    InputManager inputManager;
+
+    void Start()
+    {
+        inputManager = inputField.GetComponent<InputManager>();
+
+        text = GetComponent<Text>();
+
+    }
+
+    private void Update()
+    {
+        text.text = inputManager.inputValue;
+    }
+}
